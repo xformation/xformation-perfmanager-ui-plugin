63c39ff83c48bee21e8d45690075d8b004ff8022
Satish pal <satishweb93@gmail.com>
2020-07-08 15:08:43 +0530

    Update: Css design for dashboard page.

--- src/domain/Dashboard/index.tsx
+++ src/domain/Dashboard/index.tsx
@@ -1,16 +1,61 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { config } from '../../config';
+import { Breadcrumbs } from '../Breadcrumbs';
 
 export class Dashboard extends React.Component<any, any> {
+    breadCrumbs: any;
     constructor(props: any) {
         super(props);
         this.state = {};
+        this.breadCrumbs = [
+            {
+                label: "Home",
+                route: `/`
+            },
+            {
+                label: "Monitor | Alerts",
+                isCurrentPage: true
+            }
+        ];
     }
 
     render() {
         const state = this.state;
         return (
-            <div className="dashboard-container">
-                This is dashboard
+            <div className="perfmanager-dashboard-container">
+                <Breadcrumbs breadcrumbs={this.breadCrumbs} pageTitle="PREFORMANCE MANAGEMENT" />
+                <div className="perfmanager-page-container">
+                    <div className="common-container">
+                        <Link to={`${config.basePath}/managealertrule`} className="alert-blue-button">
+                            <i className="fa fa-cog"></i>&nbsp;&nbsp;
+                            Manage Dashboards
+                        </Link>
+                        <a className="alert-blue-button">
+                            <i className="fa fa-cog"></i>&nbsp;&nbsp;
+                            Catalog
+                        </a>
+                        <a className="alert-blue-button">
+                            <i className="fa fa-cog"></i>&nbsp;&nbsp;
+                            Library
+                        </a>
+                        <a className="alert-blue-button">
+                            <i className="fa fa-cog"></i>&nbsp;&nbsp;
+                            Collection
+                        </a>
+                        <a className="alert-blue-button">
+                            <i className="fa fa-cog"></i>&nbsp;&nbsp;
+                            Rule
+                        </a>
+                        <a className="alert-blue-button">
+                            <i className="fa fa-cog"></i>&nbsp;&nbsp;
+                            Preferences
+                        </a>
+                    </div>
+                    <div className="dashboard-metrics">
+                        <p>Metrics dashboard to be done.</p>
+                    </div>
+                </div>
             </div>
         );
     }
