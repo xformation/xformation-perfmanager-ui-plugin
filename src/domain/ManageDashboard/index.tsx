3ed645b752c768be8165fed28f82f63d4862d966
Satish pal <satishweb93@gmail.com>
2020-07-10 11:55:58 +0530

    Update: Manage Dashboard CSS design.

--- src/domain/ManageDashboard/index.tsx
+++ src/domain/ManageDashboard/index.tsx
@@ -2,7 +2,12 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { Breadcrumbs } from '../Breadcrumbs';
 import { config } from '../../config';
-
+import dashboardIcon from '../../img/dashboard-icon.png';
+import folderIcon from '../../img/folder.png';
+import listIcon from '../../img/list.png';
+import sortIcon from '../../img/sort.png';
+import tagIcon from '../../img/tag.png';
+import openFolderIcon from '../../img/open-folder.png';
 
 export class ManageDashboard extends React.Component<any, any> {
     breadCrumbs: any;
@@ -28,33 +33,232 @@ export class ManageDashboard extends React.Component<any, any> {
                 <Breadcrumbs breadcrumbs={this.breadCrumbs} pageTitle="PREFORMANCE MANAGEMENT" />
                 <div className="perfmanager-page-container">
                     <div className="common-container">
-                        <Link to={`${config.basePath}/managedashboard`} className="alert-blue-button">
-                            <i className="fa fa-cog"></i>&nbsp;&nbsp;
-                            Manage Dashboards
-                        </Link>
-                        <a className="alert-blue-button">
-                            <i className="fa fa-cog"></i>&nbsp;&nbsp;
-                            Catalog
-                        </a>
-                        <a className="alert-blue-button">
-                            <i className="fa fa-cog"></i>&nbsp;&nbsp;
-                            Library
-                        </a>
-                        <a className="alert-blue-button">
-                            <i className="fa fa-cog"></i>&nbsp;&nbsp;
-                            Collection
-                        </a>
-                        <a className="alert-blue-button">
-                            <i className="fa fa-cog"></i>&nbsp;&nbsp;
-                            Rule
-                        </a>
-                        <a className="alert-blue-button">
-                            <i className="fa fa-cog"></i>&nbsp;&nbsp;
-                            Preferences
-                        </a>
+                        <div className="row">
+                            <div className="col-lg-12 col-md-12 col-sm-12">
+                                <Link to={`${config.basePath}/managedashboards`}className="alert-blue-button">
+                                    <i className="fa fa-cog"></i>&nbsp;&nbsp;
+                                    Manage Dashboards
+                                </Link>
+                                <Link to={`${config.basePath}/catalog`} className="alert-blue-button">
+                                    <i className="fa fa-cog"></i>&nbsp;&nbsp;
+                                    Catalog
+                                </Link>
+                                <a className="alert-blue-button">
+                                    <i className="fa fa-cog"></i>&nbsp;&nbsp;
+                                    Library
+                                </a>
+                                <a className="alert-blue-button">
+                                    <i className="fa fa-cog"></i>&nbsp;&nbsp;
+                                    Collection
+                                </a>
+                                <a className="alert-blue-button">
+                                    <i className="fa fa-cog"></i>&nbsp;&nbsp;
+                                    Rule
+                                </a>
+                                <a className="alert-blue-button">
+                                    <i className="fa fa-cog"></i>&nbsp;&nbsp;
+                                    Preferences
+                                </a>
+                            </div>
+                            
+                        </div>
                     </div>
-                    <div className="dashboard-metrics">
-                        <p>Manage Dashboard.</p>
+                    <div className="common-container border-bottom-0">
+                        <div className="manage-dashboard-heading">
+                            <div className="heading-icon"><img src={dashboardIcon} alt="" /></div>
+                            <div className="heading-right">
+                                <h3>Dashboards</h3>
+                                <p>Manage dashboards & folders</p>
+                            </div>
+                        </div>
+                        <div className="manage-dashboard-radio-btns">
+                            <ul>
+                                <li>
+                                    <input type="radio" id="f-kpi" name="selector" />
+                                    <label htmlFor="f-kpi">KPI</label>
+                                </li>
+                                <li>
+                                    <input type="radio" id="f-log" name="selector" />
+                                    <label htmlFor="f-v">Log</label>
+                                </li>
+                                <li>
+                                    <input type="radio" id="f-schema" name="selector" />
+                                    <label htmlFor="f-schema">Schema</label>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="manage-dashboard-tabs">
+                            <ul>
+                                <li className="active-tab">
+                                    <a href="#">Manage</a>
+                                </li>
+                                <li>
+                                    <a href="#">Playlists</a>
+                                </li>
+                                <li>
+                                    <a href="#">Snapshots</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="manage-dashboard-search">
+                            <div className="row">
+                                <div className="col-lg-4 col-md-12 col-sm-12">
+                                    <div className="form-group search-control-group">
+                                        <form>
+                                            <input type="text" className="input-group-text" placeholder="Search dashboards by name" />
+                                            <button>
+                                                <i className="fa fa-search"></i>
+                                            </button>
+                                        </form>
+                                    </div>
+                                </div>  
+                                <div className="col-lg-8 col-md-12 col-sm-12">
+                                    <div className="search-buttons float-right">
+                                        <a className="alert-blue-button">New Dashboard</a>
+                                        <a className="alert-blue-button">New Folder</a>
+                                        <a className="alert-blue-button m-r-0">Import</a>
+                                    </div>
+                                </div>                          
+                            </div>
+                        </div>
+                        <div className="manage-dashboard-fliter-sort">
+                            <div className="row">
+                                <div className="col-lg-6 col-md-12 col-sm-12">
+                                    <div className="sort-checkbox">
+                                        <input type="checkbox" className="checkbox" />
+                                    </div>
+                                    <div className="sort-view">
+                                        <ul>
+                                            <li className="active">
+                                                <a href="#"><img src={folderIcon} alt="" /></a>
+                                            </li>
+                                            <li>
+                                                <a href="#"><img src={listIcon} alt="" /></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div className="sort-select-menu">
+                                        <span>
+                                            <img src={sortIcon} alt="" />
+                                        </span>
+                                        <select>
+                                            <option>Sort (Default A-Z)</option>
+                                            <option>Sort (Default A-Z)</option>
+                                            <option>Sort (Default A-Z)</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="col-lg-6 col-md-12 col-sm-12">
+                                    <div className="filter-starred float-right">
+                                        <div className="sort-checkbox">
+                                            <input type="checkbox" className="checkbox" />
+                                            <span>
+                                                Filter by starred
+                                            </span>
+                                        </div>
+                                        <div className="sort-select-menu">
+                                            <span>
+                                                <img src={tagIcon} alt="" />
+                                            </span>
+                                            <select>
+                                                <option>Filter by tag</option>
+                                                <option>Filter by tag</option>
+                                                <option>Filter by tag</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="manage-dashboard-general">
+                            <div className="general-heading">
+                                <input type="checkbox" className="checkbox" />
+                                <span><img src={openFolderIcon} alt="" /></span>
+                                <h4>General</h4>
+                                <i className="fa fa-angle-down float-right"></i>
+                            </div>
+                            <div className="general-logs">
+                                <table className="data-table">
+                                    <tr>
+                                        <td>
+                                            <input type="checkbox" className="checkbox" /> 
+                                        </td>
+                                        <td>
+                                            <span>Amazon CloudWatch Logs</span>
+                                        </td>
+                                        <td>
+                                            <div className="float-right">
+                                                <div className="aws-bg">AWS</div>
+                                                <div className="amazon-bg">Amazon</div>
+                                                <div className="cloudwatch-bg">CloudWatch</div>
+                                                <div className="logs-bg">Logs</div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <input type="checkbox" className="checkbox" /> 
+                                        </td>
+                                        <td>
+                                            <span>Amazon RDS</span>
+                                        </td>
+                                        <td>
+                                            <div className="float-right">
+                                                <div className="cloudwatch-bg">CloudWatch</div>
+                                                <div className="monitorigartist-bg">monitorigartist</div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <input type="checkbox" className="checkbox" /> 
+                                        </td>
+                                        <td>
+                                            <span>AWS VPN</span>
+                                        </td>
+                                        <td>
+                                            <div className="float-right">
+                                                <div className="cloudwatch-bg">CloudWatch</div>
+                                                <div className="monitorigartist-bg">monitorigartist</div>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <input type="checkbox" className="checkbox" /> 
+                                        </td>
+                                        <td>
+                                            <span>AWS VPN Dashboard</span>
+                                        </td>
+                                        <td>
+                                            
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <input type="checkbox" className="checkbox" /> 
+                                        </td>
+                                        <td>
+                                            <span>Cloud Trial</span>
+                                        </td>
+                                        <td>
+                                            
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <input type="checkbox" className="checkbox" /> 
+                                        </td>
+                                        <td>
+                                            <span>Cloud Watch</span>
+                                        </td>
+                                        <td>
+                                            
+                                        </td>
+                                    </tr>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
