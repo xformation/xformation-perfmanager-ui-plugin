d3fa1ed60d48f5c9f5fef6648956b7170c901497
manojkrjoshi <44704973+manojkrjoshi@users.noreply.github.com>
2020-07-22 10:57:50 +0530

    Catalog UI changes

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,10 @@
+const IP = "localhost";
+
+const securitySrvUrl = `http://${IP}:8094`;
+const catalogSrvUrl = `http://${IP}:4000/api`;
+
 export const config = {
-    basePath: "/plugins/xformation-perfmanager-ui-plugin/page"
+  basePath: "/plugins/xformation-perfmanager-ui-plugin/page",
+  GET_ALL_COLLECTOR: `${catalogSrvUrl}/listCollector`,
+  PERMS_LIST_ALL: `${securitySrvUrl}/security/permissions/listAll`
 };
