0947c1670f8a46c48a1fd4cf3d99cc56253da10b
Satish Pal <satishweb93@gmail.com>
2021-01-01 17:13:38 +0530

    Update: UI for NewDashboard in manage dashboard.

--- src/domain/ManageDashboard/NewDashboard.tsx
+++ src/domain/ManageDashboard/NewDashboard.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import Rbac from '../../components/Rbac';
 import { config } from '../../config';
-import folderIcon from '../../img/folder.png';
+import createDashboardImage from '../../img/create-dashboard-image.png';
+import importDashboardImage from '../../img/import-dashboard-image.png';
 export class NewDashboard extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
@@ -18,22 +19,22 @@ export class NewDashboard extends React.Component<any, any> {
         return (
             <div className="newdashboard-container">
                 <div className="general-heading">
-                    <div className="row">
-                        <div className="col-lg-12 col-md-12 col-sm-12">
-                            <h4>New Dashboard</h4>
-                            <span><i className="fa fa-close" onClick={this.closenewDashboard}></i></span>
-                        </div>
-                    </div>
+                    <h4>New Dashboard</h4>
+                    <span className="newdashboard-close"><i className="fa fa-close" onClick={this.closenewDashboard}></i></span>
                 </div>
                 <div className="general-center-contain">
-                    <div className="row own-dashboard">
-                        <div className="col-lg-6 col-md-6 col-sm-6">
-                            <img src={folderIcon} alt="" />
+                    <div className="row">
+                        <div className="col-lg-6 col-md-6 col-sm-12 own-dashboard">
+                            <span>
+                                <img src={createDashboardImage} alt="Create your own dashboard" />
+                            </span>
                             <label>Create your own dashboard</label>
                         </div>
-                        <div className="col-lg-6 col-md-6 col-sm-6">
-                            <img src={folderIcon} alt="" />
-                            <label>Import Dashboard from Catalog </label>
+                        <div className="col-lg-6 col-md-6 col-sm-12 own-dashboard">
+                            <span>
+                                <img src={importDashboardImage} alt="Import Dashboard from Catalog" />
+                            </span>
+                            <label>Import Dashboard from Catalog</label>
                         </div>
                     </div>
                 </div>
