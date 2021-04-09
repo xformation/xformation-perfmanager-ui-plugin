d23de53c0ae9b7ae223083d8924191308d99085f
Satish Pal <satishweb93@gmail.com>
2021-04-09 09:39:19 +0530

    Added PreviewDashboard iframe url in catalog

--- src/domain/Catalog/PreviewDashboard.tsx
+++ src/domain/Catalog/PreviewDashboard.tsx
@@ -23,7 +23,7 @@ export class PreviewDashboard extends React.Component<any, any> {
                 <ModalHeader toggle={this.toggle}>AWS Config App Dashboards- Preview</ModalHeader>
                 <ModalBody style={{ height: 'calc(90vh - 150px)', overflowY: 'auto', overflowX: "hidden" }}>
                     {/* <img src={previewDashboardimage} alt="Open Folder" /> */}
-                    <iframe src="http://localhost:8888/catalogue/1/" width="100%" height="98%" frameBorder="0"></iframe>
+                    <iframe src="https://wonderful-thompson-86244c.netlify.app/catalogue/this-is-catalogue-1/" width="100%" height="98%" frameBorder="0"></iframe>
                 </ModalBody>
             </Modal>
         );
